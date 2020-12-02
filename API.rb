94014da9214f3b4268efbfad6a73c5dc20552b02
Walter Aab <aabwalter@yahoo.com>
2020-12-01 20:01:32 -0600

    Add basic data retrieve function

--- API.rb
+++ API.rb
@@ -1,7 +1,10 @@
-BASE_URL = "https://developer.nps.gov/api/v1/parks"
-park_code = "?parkCode=acad&api_key=#{ENV['API_KEY']}"
+require_relative './environment'
 
-uri = URI(BASE_URL + park_code)
+BASE_URL = "https://developer.nps.gov/api/v1/parks"
+park_code = "?parkCode=acad&api_key="
+key = "#{ENV['API_KEY']}"
+
+uri = URI(BASE_URL + park_code + key)
 response = Net::HTTP.get(uri)
 formatted_resp = JSON.parse(response)
 binding.pry
